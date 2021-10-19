31f58cf3d4f4cb9c90a7624a1eeedf207fa66b40
alonchoong <aronchoong@gmail.com>
2021-10-20 00:31:28 +0800

    Initial commit

---
+++ src/main/java/com/shopit/sellermicroservice/SellerMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.shopit.sellermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellerMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellerMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopit/sellermicroservice/SellerMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopit.sellermicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellerMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
